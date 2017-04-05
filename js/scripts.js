e7b891a81bf49bad115bb76e548fcfa8a85c5376
Aaron Nguyen and Ben Petrosky <aaron@>
2017-04-05 09:41:04 -0700

    add initial html, functioning user and business logic generating a random number

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var randomNumber = function(){
+
+  var diceArray = [1,2,3,4,5,6];
+  var rNumber = diceArray[Math.floor(Math.random() * diceArray.length)];
+  return rNumber;
+}
+
+
+$(function(){
+  $("#form-one").submit(function(event){
+    event.preventDefault();
+
+    var userTurn = randomNumber();
+    console.log(userTurn)
+  });
+})
